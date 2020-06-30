3690c8c07b012b3656af0394a042502620cb98d5
Aneta Szynal <anetaszynal28@gmail.com>
2020-06-30 16:54:45 +0200

    feat(js):  remove tasks

--- js/script.js
+++ js/script.js
@@ -1,42 +1,57 @@
 {
-    
-
     const tasks = [];
+
+    const addNewTask = (newTask) => {
+        tasks.push({
+            content: newTask,
+        });
+
+        render();
+    }
+
+    const deleteTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+
+        render();
+    }
 
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
-            <li>${task.content}
+            <li class="list__items"><button class="list__button list__button--done"></button>${task.content}<button class="list__button list__button--deleted js-deleteTask"></button>
             </li>`;
         }
         document.querySelector(".js-taskList").innerHTML = htmlString;
+
+        const deleteTaskButtons = document.querySelectorAll(".js-deleteTask");
+
+        deleteTaskButtons.forEach((deleteTaskButton, index) => {
+            deleteTaskButton.addEventListener("click", () => {
+                deleteTask(index);
+
+            });
+
+        });
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTask = document.querySelector(".js-enterNewTask").value;
+
+        if (newTask === "") {
+            return;
+        }
+        addNewTask(newTask);
     }
 
 
     const init = () => {
-        
-
         const formElement = document.querySelector(".js-form");
-        formElement.addEventListener("submit", (event) => {
-        event.preventDefault();
 
-        const newTask = document.querySelector(".js-enterNewTask").value;
-
-        if (newTask === "") {
-            return
-        }
-
-        tasks.push({
-            content: newTask,
-        });
-
-        render();
-
-        });      
-        
-    }
+        formElement.addEventListener("submit", onFormSubmit);
+    };
 
     init();
 }
